9238f9910be81dc4f109480b60652b01a9caff45
Tayler Schanks <taylerschanks2@gmail.com>
2023-03-30 21:01:54 -0500

    lab objective

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  console.log(drivers)
+  console.log(string)
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  console.log(drivers)
+  console.log(string)
+  return drivers.filter(function(driver) {
+    return driver.indexOf(string) === 0
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name  === string
+  })
+}
